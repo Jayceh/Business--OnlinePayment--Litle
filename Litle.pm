242c5f4877b4103de8ca4908e7bf6c30a70c2c07
Jason (Jayce^) Hall <jayce@lug-nut.com>
2012-10-08 16:15:19 -0600

    Version 0.932

--- Litle.pm
+++ Litle.pm
@@ -21,7 +21,7 @@ use Log::Scrubber qw(disable $SCRUBBER scrubber :Carp);
 @ISA     = qw(Business::OnlinePayment::HTTPS);
 $me      = 'Business::OnlinePayment::Litle';
 $DEBUG   = 0;
-$VERSION = '0.931';
+$VERSION = '0.932';
 
 =head1 NAME
 
@@ -29,7 +29,7 @@ Business::OnlinePayment::Litle - Litle & Co. Backend for Business::OnlinePayment
 
 =head1 VERSION
 
-Version 0.931
+Version 0.932
 
 =cut
 
